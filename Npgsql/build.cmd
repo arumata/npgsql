0b56f13dd1e1ddd41c2aa75841c0faa4c9a23f25
Sergey Adeninskiy <adeninskiy@akura.ru>
2017-08-10 12:24:57 +0300

    building

--- Npgsql/build.cmd
+++ Npgsql/build.cmd
@@ -1,2 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat" 
 MSBuild Npgsql.csproj /t:Rebuild /p:Configuration=Release-net40
 nuget.exe pack Npgsqlp.nuspec -OutputDirectory %NUGET_LOCAL%
